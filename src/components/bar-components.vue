3d898adb5d849acc4d3900c6a4befd8d3a57c23e
naga <cdevns@gmail.com>
2020-04-06 20:06:47 +0530

    shares reorg

--- src/components/bar-components.vue
+++ src/components/bar-components.vue
@@ -2,10 +2,27 @@
   <div>
     <BarAxis />
     <hr />
+
+    <!-- BAR BASIC -->
     <BarBasic />
-    <hr/>
-    <BarFilter />
     <hr />
+
+    <!-- BAR GRAPH FILTERS -->
+    <form>
+      <div class="row">
+        <div class="col-xs-12 form-group">
+          <label for="selectOptions" class="col-xs-2">Select Filter</label>
+          <select id="selectOptions" class="col-xs-3" @change="onChange">
+            <option value="select">select</option>
+            <option v-for="(option,index) in selectOptions" :key="index">{{option}}</option>
+          </select>
+        </div>
+      </div>
+    </form>
+    <BarFilter :barGraphData="filteredData" />
+    <hr />
+
+    <!-- BAR HOVER -->
     <BarHover />
     <hr />
     <BarTooltip />
@@ -13,6 +30,8 @@
 </template>
 
 <script>
+import * as d3 from "d3";
+
 import BarAxis from "./data-visualization/bar-graph/bar-graph-axis.vue";
 import BarBasic from "./data-visualization/bar-graph/bar-graph-basic.vue";
 import BarFilter from "./data-visualization/bar-graph/bar-graph-filters.vue";
@@ -26,6 +45,23 @@ export default {
     BarFilter,
     BarHover,
     BarTooltip
+  },
+  data() {
+    return {
+      selectOptions: ["Books", "Journals"],
+      filteredData: []
+    };
+  },
+  methods: {
+    onChange() {
+      console.log("onchange: ", event.target.value);
+      this.fetchData();
+    },
+    async fetchData() {
+      let data = await d3.json("./data/books.json");
+      console.log("books-data: ", data);
+      this.filteredData = data;
+    }
   }
 };
 </script>
--- src/components/data-visualization/bar-graph/bar-graph-filters.vue
+++ src/components/data-visualization/bar-graph/bar-graph-filters.vue
@@ -1,17 +1,7 @@
 <template>
   <div>
     <h2>BAR GRAPH FILTER</h2>
-    <form>
-      <div class="row">
-        <div class="col-xs-12 form-group">
-          <label for="selectOptions" class="col-xs-2">Select Filter</label>
-          <select id="selectOptions" class="col-xs-3" @change="onChange">
-            <option value="select">select</option>
-            <option v-for="(option,index) in selectOptions" :key="index">{{option}}</option>
-          </select>
-        </div>
-      </div>
-    </form>
+
     <svg id="bar-filter" :width="width" :height="height" />
   </div>
 </template>
@@ -21,111 +11,107 @@ import * as d3 from "d3";
 export default {
   data() {
     return {
-      // dataset: [12, 31, 22, 17, 25, 18, 29, 14, 9],
       width: 600,
-      height: 200,
-      selectOptions: ["Books", "Journals"],
-      bookList: []
+      height: 200
     };
   },
   methods: {
-    onChange() {
-      console.log("onchange: ", event.target.value);
-      this.fetchData();
-    },
-    async fetchData() {
-      let data = await d3.json("./data/books.json");
-      console.log("books-data: ", data);
-      this.bookList = data;
+    drawBarGraph(graphData) {
+      var margin = { left: 100, top: 10, right: 10, bottom: 70 };
+
+      var width = this.width - margin.left - margin.right;
+      var height = this.height - margin.top - margin.bottom;
+
+      const svg = d3
+        .select("#bar-filter")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var xscale = d3
+        .scaleBand()
+        .domain(
+          graphData.map(function(d) {
+            return d.sold_count;
+          })
+        )
+        .range([0, width])
+        .padding(0.3);
+
+      var xAxisCall = d3.axisBottom(xscale).tickFormat(function(d, i) {
+        return i;
+      });
+
+      svg
+        .append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxisCall);
+
+      svg
+        .append("text")
+        .attr("class", "x-axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + 50)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .text("X Axis");
+
+      var yscale = d3
+        .scaleLinear()
+        .domain([
+          0,
+          d3.max(graphData, function(book) {
+            return book.sold_count;
+          })
+        ])
+        .range([height, 0]);
+
+      var yAxisCall = d3.axisLeft(yscale).ticks(3);
+
+      svg
+        .append("g")
+        .attr("class", "y axis")
+        .call(yAxisCall);
+
+      svg
+        .append("text")
+        .attr("class", "y-axis-label")
+        .attr("x", -(height / 2))
+        .attr("y", -50)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("Y Axis");
+
+      svg
+        .selectAll("rect")
+        .data(graphData)
+        .enter()
+        .append("rect")
+        .attr("x", function(d) {
+          return xscale(d.sold_count);
+        })
+        .attr("y", function(d) {
+          return yscale(d.sold_count);
+        })
+        .attr("width", xscale.bandwidth)
+        .attr("height", function(d) {
+          return height - yscale(d.sold_count);
+        })
+        .attr("fill", "grey");
     }
   },
   mounted: function() {
-    var margin = { left: 100, top: 10, right: 10, bottom: 70 };
-
-    var width = this.width - margin.left - margin.right;
-    var height = this.height - margin.top - margin.bottom;
-
-    const svg = d3
-      .select("#bar-filter")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var xscale = d3
-      .scaleBand()
-      .domain(
-        this.bookList.map(function(d) {
-          console.log("domain");
-          return d.sold_count;
-        })
-      )
-      .range([0, width])
-      .padding(0.3);
-
-    var xAxisCall = d3.axisBottom(xscale).tickFormat(function(d, i) {
-      return i;
-    });
-
-    svg
-      .append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxisCall);
-
-    svg
-      .append("text")
-      .attr("class", "x-axis-label")
-      .attr("x", width / 2)
-      .attr("y", height + 50)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .text("X Axis");
-
-    var yscale = d3
-      .scaleLinear()
-      .domain([
-        0,
-        d3.max(this.bookList, function(book) {
-          return book.sold_count;
-        })
-      ])
-      .range([height, 0]);
-
-    var yAxisCall = d3.axisLeft(yscale).ticks(3);
-
-    svg
-      .append("g")
-      .attr("class", "y axis")
-      .call(yAxisCall);
-
-    svg
-      .append("text")
-      .attr("class", "y-axis-label")
-      .attr("x", -(height / 2))
-      .attr("y", -50)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .attr("transform", "rotate(-90)")
-      .text("Y Axis");
-
-    svg
-      .selectAll("rect")
-      .data(this.bookList)
-      .enter()
-      .append("rect")
-      .attr("x", function(d) {
-        return xscale(d.sold_count);
-      })
-      .attr("y", function(d) {
-        return yscale(d.sold_count);
-      })
-      .attr("width", xscale.bandwidth)
-      .attr("height", function(d) {
-        return height - yscale(d.sold_count);
-      })
-      .attr("fill", "grey");
+    this.drawBarGraph(this.barGraphData);
+  },
+  props: ["barGraphData"],
+  watch: {
+    barGraphData: function(newData) {
+      this.drawBarGraph(newData);
+    }
   }
 };
 </script>
